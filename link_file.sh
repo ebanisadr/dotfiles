ee9754905fc8ae4851031e8fc14668a199b973d4
Eric Banisadr <ebani@uw.edu>
2019-11-04 15:50:17 -0800

    Make linking output more conscise

--- link_file.sh
+++ link_file.sh
@@ -3,5 +3,5 @@
 if [ -e ~/.$1 ]; then
   echo $1 found, skipping
 else
-ln -s $(pwd)/$1 ~/.$1 && echo $1 linked to ~/.$1 || echo Error linking $1
+ln -s $(pwd)/$1 ~/.$1 && echo $1 linked to '~/'.$1 || echo Error linking $1
 fi
